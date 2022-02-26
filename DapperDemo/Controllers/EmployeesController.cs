637083555693b537c1f468cf155293bf8a7cdee5
ozknbaklaci <ozkan-baklaci@hotmail.com>
2022-02-26 17:14:25 +0300

    Employee Controller And UI

---
+++ DapperDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,94 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DapperDemo.Models;
+using DapperDemo.Repository;
+
+namespace DapperDemo.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ICompanyRepository _companyRepository;
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository,
+            ICompanyRepository companyRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _companyRepository = companyRepository;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            return View(await _employeeRepository.GetAll());
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                await _employeeRepository.Add(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _employeeRepository.Find(id.GetValueOrDefault());
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Employee employee)
+        {
+            if (id != employee.EmployeeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _employeeRepository.Update(employee);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            await _employeeRepository.Remove(id.GetValueOrDefault());
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
